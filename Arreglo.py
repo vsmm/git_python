100ee43a34dd24c718d582580a46253bff00a6de
victor Martinez <victor.salvador.martinez.94@gmail.com>
2016-10-07 14:46:18 -0500

    Script arreglo en python

---
+++ Arreglo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#-*-coding: utf-8 -*-
+
+
+from array import array
+
+miArreglo = array("i", [10,20,30])
+i = 0
+
+for value in miArreglo:
+	print("miArreglo[{0}] es {1}" .format(i,value))
+	i += 1
