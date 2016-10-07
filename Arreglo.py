1f8ee2c5be19836ab2e0425bca44d5082bedcf82
victor Martinez <victor.salvador.martinez.94@gmail.com>
2016-10-07 14:58:32 -0500

    Se agrego un print

--- Arreglo.py
+++ Arreglo.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 #-*-coding: utf-8 -*-
-
-
 from array import array
 
 miArreglo = array("i", [10,20,30])
 i = 0
-
+print("hola")
 for value in miArreglo:
 	print("miArreglo[{0}] es {1}" .format(i,value))
 	i += 1
