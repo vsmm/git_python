abd2556ba624c8e682c9b3cf10183c6cdefb9fe7
victor Martinez <victor.salvador.martinez.94@gmail.com>
2016-10-19 00:48:01 -0500

    correccion en la concatenacion

--- Sensor_temp.py
+++ Sensor_temp.py
@@ -15,5 +15,5 @@ def get_temp_sens():
 i = 1
 while i <= 30:
     mensaje = str(get_temp_sens()) + " ºC"
-    print("La temperatura es: " mensaje)
+    print("La temperatura es: " + mensaje)
     i += 1
