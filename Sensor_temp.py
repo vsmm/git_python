e340ab6ac58dd22ffd361915ed1c0d8af9417c0f
victor Martinez <victor.salvador.martinez.94@gmail.com>
2016-10-19 00:30:24 -0500

    modificando para que no pare el script

---
+++ Sensor_temp.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+def get_temp_sens():
+        tfile = open("/sys/bus/w1/devices/28-01157169e3ff/w1_slave")
+        text = tfile.read()
+        tfile.close()
+        secondline = text.split("\n")[1]
+        temperaturedata = secondline.split(" ")[9]
+        temperature = float(temperaturedata[2:])
+        temperature = temperature / 1000
+        return float(temperature)
+mensaje = str(get_temp_sens()) + " ºC"
+
+i = 1
+while i <= 3:
+    print(mensaje)
+    i += 1
