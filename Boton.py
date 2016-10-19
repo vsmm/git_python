0d9c35271dd12fc1c8bdbc69e89b86e6ce5ba54b
victor Martinez <victor.salvador.martinez.94@gmail.com>
2016-10-19 11:24:00 -0500

    Se agrego un print

--- Boton.py
+++ Boton.py
@@ -14,7 +14,7 @@ try:
         print 'Contador inicial:', contador
         if GPIO.input(27):
             GPIO.output (17, 1)
-            #contador = contador + 1
+            contador = contador + 1
             #print 'Contador:', contador
         else:
             GPIO.output(17,0)
