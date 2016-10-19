3355c7604cfeeee39bcd608de7ede1d23062def3
victor Martinez <victor.salvador.martinez.94@gmail.com>
2016-10-19 00:36:59 -0500

    script

--- Sensor_temp.py
+++ Sensor_temp.py
@@ -9,16 +9,10 @@ def get_temp_sens():
         temperature = float(temperaturedata[2:])
         temperature = temperature / 1000
         return float(temperature)
-<<<<<<< HEAD
 
-mensaje = str(get_temp_sens()) + " ºC"
-
-print(mensaje)
-=======
 mensaje = str(get_temp_sens()) + " ºC"
 
 i = 1
 while i <= 3:
     print(mensaje)
     i += 1
->>>>>>> prueba
