146931b3e28bd8939eb0ef9eda236a31b6629fc9
victor Martinez <victor.salvador.martinez.94@gmail.com>
2016-10-25 17:57:48 -0500

    Falto los = en las asignaciones

--- Piano_buzzer.py
+++ Piano_buzzer.py
@@ -4,42 +4,42 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 
 #frecuencias
-cL 129
-cLS 139
-dL 146
-dLS 156
-eL 163
-fL 173
-fLS 185
-gL 194
-gLS 207
-aL 219
-aLS 228
-bL 232
-c 261
-cS 277
-d 294
-dS 311
-e 329
-f 349
-fS 370
-g 391
-gS 415
-a 440
-aS 455
-b 466
-cH 523
-cHS 554
-dH 587
-dHS 622
-eH 659
-fH 698
-fHS 740
-gH 784
-gHS 830
-aH 880
-aHS 910
-bH 933
+cL = 129
+cLS = 139
+dL = 146
+dLS = 156
+eL = 163
+fL = 173
+fLS = 185
+gL = 194
+gLS = 207
+aL = 219
+aLS = 228
+bL = 232
+c = 261
+cS = 277
+d = 294
+dS = 311
+e = 329
+f = 349
+fS = 370
+g = 391
+gS = 415
+a = 440
+aS = 455
+b = 466
+cH = 523
+cHS = 554
+dH = 587
+dHS = 622
+eH = 659
+fH = 698
+fHS = 740
+gH = 784
+gHS = 830
+aH = 880
+aHS = 910
+bH = 933
 
 #esta funcion es para hacer la onda cuadrada de la senial que el buzzer usa
 def beep(nota, duracion):
